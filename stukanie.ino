1dc1c784fbd9fd9750a077eec1c83a2359bd8039
Marek Szulik <marek.szulik@gmail.com>
2019-04-24 13:29:22 +0200

    Simplify the code and add comments

--- stukanie.ino
+++ stukanie.ino
@@ -1,45 +1,37 @@
-#include "lib/Led/Led.cpp"
-#include "lib/Buzzer/Buzzer.cpp"
-
 #include <Stepper.h>
 
+// My stepper motor is apparently designed to have 64 steps (max speed), but I _think_ setting the steps to 128
+// makes it faster.
 #define STEPS 128
 
-const int IDLE_TIME = 180 * 1000;
-const int RIDE_TIME = 30 * 1000;
-const int SOUND_SENSOR_PIN = 7;
+// Since we don't want the robot to listen to sounds while we fall asleep, we set an initial delay of 4 hours
+const long INITIAL_DELAY = 4 * 60 * 60 * 1000L;
+
+// The robot should not move if it moved in the last 3 minutes.
+const long IDLE_TIME = 180 * 1000L;
+
 const int MOVEMENT_DISTANCE = 15000;
+
+const int SOUND_SENSOR_PIN = 7;
 
 Stepper stepper (STEPS, 2, 4, 3, 5);
 
 long lastSoundTime = 0;
-bool shouldMove = false;
-bool moving = false;
-long movementForwardTime = 0;
 
 void setup() {
   pinMode(SOUND_SENSOR_PIN, INPUT);
-  stepper.setSpeed(200);
+  stepper.setSpeed(STEPS);
 }
 
 void loop() {
   int soundHigh = digitalRead(SOUND_SENSOR_PIN);
-  if (!shouldMove && soundHigh && (millis() - lastSoundTime > IDLE_TIME || lastSoundTime == 0)) {
+  if (soundHigh && isTimeConstraintMet()) {
     lastSoundTime = millis();
-    shouldMove = true;
+    stepper.step(-MOVEMENT_DISTANCE);
+    stepper.step(MOVEMENT_DISTANCE);
   }
+}
 
-  if (shouldMove) {
-    if (!moving) {
-      moving = true;
-      movementForwardTime = millis();
-      stepper.step(-MOVEMENT_DISTANCE);
-      stepper.step(MOVEMENT_DISTANCE);
-    }
-
-    if (moving && millis() - movementForwardTime > RIDE_TIME) {
-      shouldMove = false;
-      moving = false;
-    }
-  }
+bool isTimeConstraintMet() {
+  return millis() > INITIAL_DELAY && millis() - lastSoundTime > IDLE_TIME;
 }
